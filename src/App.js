4705bdfe6c20721ede3e2395ff963539b99fff42
Grant Belcher <gtbelch@gmail.com>
2022-03-24 12:19:13 -0700

    establishes basic react envm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+import piazza from './piazza.jpeg'
+
+const App = () => (
+  <div>
+    <h1 className="wow">Hello React!!!!</h1>
+    <img src={piazza} alt=""/>
+  </div>
+);
+
+export default App;
