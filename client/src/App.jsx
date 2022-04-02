ed3110ac1e6fb97649e7f820d9aece439e5740bd
Grant Belcher <gtbelch@gmail.com>
2022-04-02 09:25:53 -0700

    links global styles sheet to html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Test from "./components/Test/Test";
 import piazza from "./piazza.jpeg";
 
--- client/src/components/Test/Test.jsx
+++ client/src/components/Test/Test.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import styles from "./Test.module.css";
 
 const Test = () => {
-  console.log("test");
-  return (
-    <div className={`${styles.greenText} ${styles.salmon}`}>
-      test content appears here
-    </div>
-  );
+  return <div className={styles.salmon}>test content appears here</div>;
 };
 
 export default Test;
