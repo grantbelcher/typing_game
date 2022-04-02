653d9672dec44b9193af27dcd7b422e65d3e5c02
Grant Belcher <gtbelch@gmail.com>
2022-04-02 09:45:34 -0700

    creates basic layout for App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Test from "./components/Test/Test";
-import piazza from "./piazza.jpeg";
 
 const App = () => (
   <div>
-    <h1 className="wow">Welcome to React!</h1>
-    <Test />
-    <img src={piazza} alt="" />
+    <h1 className="wow">How Fast Can You Type?</h1>
+    <textarea />
+    <h4>Time Remaining: 60 seconds</h4>
+    <button>Start Game</button>
+    <h1>Word Count: X words</h1>
   </div>
 );
 
