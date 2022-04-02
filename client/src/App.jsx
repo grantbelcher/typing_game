f84017446712845992223bb2e8ed88b8b8cde505
Grant Belcher <gtbelch@gmail.com>
2022-04-02 09:57:39 -0700

    creates handleTyping to store textarea value in state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TextBox from "./components/text_box/TextBox";
 
-const App = () => (
-  <div>
-    <h1 className="wow">How Fast Can You Type?</h1>
-    <h4>Time Remaining: 60 seconds</h4>
-    <TextBox />
-    <button>Start Game</button>
-    <h1>Word Count: X words</h1>
-  </div>
-);
+const App = () => {
+  const [text, setText] = useState("");
+
+  const handleTyping = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className="wow">How Fast Can You Type?</h1>
+      <h4>Time Remaining: 60 seconds</h4>
+      <TextBox handleChange={handleTyping} text={text} />
+      <button>Start Game</button>
+      <h1>Word Count: X words</h1>
+    </div>
+  );
+};
 
 export default App;
--- client/src/components/text_box/TextBox.jsx
+++ client/src/components/text_box/TextBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const TextBox = () => {
-  return <textarea />;
+const TextBox = ({ handleChange, text }) => {
+  return <textarea onChange={(e) => handleChange(e)} value={text} />;
 };
 
 export default TextBox;
