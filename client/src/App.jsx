2c86c0206f317a0246dbe6675d838438c866dd24
Grant Belcher <gtbelch@gmail.com>
2022-04-02 09:51:16 -0700

    moves textarea to separate component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import TextBox from "./components/text_box/TextBox";
 
 const App = () => (
   <div>
     <h1 className="wow">How Fast Can You Type?</h1>
-    <textarea />
     <h4>Time Remaining: 60 seconds</h4>
+    <TextBox />
     <button>Start Game</button>
     <h1>Word Count: X words</h1>
   </div>
