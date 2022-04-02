a0ef43e20265f1df5eed4f2536f8337b1db49c94
Grant Belcher <gtbelch@gmail.com>
2022-04-02 10:23:32 -0700

    button initiates countdown of time remaining

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextBox from "./components/text_box/TextBox";
 
 const App = () => {
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(60);
+  const [timeRemaining, setTimeRemaining] = useState(5);
   const [gameIsActive, setGameIsActive] = useState(false);
+
+  useEffect(() => {
+    if (gameIsActive && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    }
+  }, [gameIsActive, timeRemaining]);
 
   const handleTyping = (event) => {
     setText(event.target.value);
   };
 
   const toggleGameIsActive = () => {
-    console.log("yo");
     gameIsActive ? setGameIsActive(false) : setGameIsActive(true);
   };
 
   return (
     <div>
       <h1 className="wow">How Fast Can You Type?</h1>
-      <h4>Time Remaining: 60 seconds</h4>
+      <h4>Time Remaining: {timeRemaining} seconds</h4>
       <TextBox handleChange={handleTyping} text={text} />
       <button onClick={toggleGameIsActive}>Start Game</button>
       <h1>Word Count: X words</h1>
